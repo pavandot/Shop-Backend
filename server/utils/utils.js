c2f72079da1538f18ac887f480a7ca8adb5b85da
pavan <pavandotnet007@outlook.com>
2022-06-07 14:42:28 +0530

    brand routes added

--- server/utils/utils.js
+++ server/utils/utils.js
@@ -2,6 +2,5 @@ import Brand from '../models/brandModel.js';
 
 export const brandCustomEnumFunction = async (v) => {
 	const brand = await Brand.findOne({ name: v });
-	console.log(brand);
 	return !!brand;
 };
