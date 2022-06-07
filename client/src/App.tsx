6faf5d761d1cc6178adf3037662346f6642b2f8c
pavan <pavandotnet007@outlook.com>
2022-06-07 18:41:42 +0530

    package updated

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,45 +1,48 @@
-import { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import { useEffect, useState } from 'react';
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+	const [count, setCount] = useState(0);
+	const [product, setProduct] = useState([]);
+	useEffect(() => {
+		const fetchProducts = async () => {
+			const response = await fetch('/products');
+			console.log(response);
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
-    </div>
-  )
+			const data = await response.json();
+			setProduct(data);
+			console.log(data);
+		};
+		fetchProducts();
+	}, []);
+	console.log(product);
+
+	return (
+		<div className='App'>
+			<header className='App-header'>
+				<img src={logo} className='App-logo' alt='logo' />
+				<p>Hello Vite + React!</p>
+				<p>
+					<button type='button' onClick={() => setCount((count) => count + 1)}>
+						count is: {count}
+					</button>
+				</p>
+				<p>
+					Edit <code>App.tsx</code> and save to test HMR updates.
+				</p>
+				<p>
+					<a className='App-link' href='https://reactjs.org' target='_blank' rel='noopener noreferrer'>
+						Learn React
+					</a>
+					{' | '}
+					<a className='App-link' href='https://vitejs.dev/guide/features.html' target='_blank' rel='noopener noreferrer'>
+						Vite Docs
+					</a>
+				</p>
+			</header>
+		</div>
+	);
 }
 
-export default App
+export default App;
