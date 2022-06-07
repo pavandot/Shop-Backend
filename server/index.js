e090c35ff710858628d5ae043560076af008d825
pavan <pavandotnet007@outlook.com>
2022-06-07 18:04:35 +0530

    package updated

--- server/index.js
+++ server/index.js
@@ -33,8 +33,8 @@ app.use('/categories', categoryRoutes);
 
 // Serve frontend
 if (process.env.NODE_ENV === 'production') {
-	app.use(express.static(path.join(__dirname, '../client/build')));
-	app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, '../', 'client', 'build', 'index.html')));
+	app.use(express.static(path.join(__dirname, '../client/dist')));
+	app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, '../', 'client', 'dist', 'index.html')));
 } else {
 	app.get('/', (req, res) => res.send('Please set to production'));
 }
