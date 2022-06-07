239d4ac9555bd2a6db7a69b71095f00b37490add
pavan <pavandotnet007@outlook.com>
2022-06-07 20:43:41 +0530

    converted to cra

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,13 @@
+import { useEffect } from 'react';
 function App() {
+	useEffect(() => {
+		const fetchData = async () => {
+			const response = await fetch('/products');
+			const json = await response.json();
+			console.log(json);
+		};
+		fetchData();
+	}, []);
 	return (
 		<section>
 			<h1>Hello</h1>
