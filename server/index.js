522e978a8f97f5e9e929d16c05d45984fccc215b
pavan <pavandotnet007@outlook.com>
2022-06-07 16:40:01 +0530

    added client

--- server/index.js
+++ server/index.js
@@ -15,6 +15,8 @@ dotenv.config();
 const port = process.env.PORT || 5000;
 const app = express();
 
+connectDB();
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
@@ -25,8 +27,15 @@ app.use('/wishlist', wishlistRoutes);
 app.use('/brand', brandRoutes);
 app.use('/categories', categoryRoutes);
 
+// Serve frontend
+if (process.env.NODE_ENV === 'production') {
+	app.use(express.static(path.join(__dirname, '../client/build')));
+	app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, '../', 'client', 'build', 'index.html')));
+} else {
+	app.get('/', (req, res) => res.send('Please set to production'));
+}
+
 app.use(errorHandler);
-connectDB();
 app.listen(port, () => {
 	console.log(`Server is running on port ${port}`);
 });
