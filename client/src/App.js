566afe41a2357f30c3f90fbb6be0e6fac849fb21
pavan <pavandotnet007@outlook.com>
2022-06-07 20:48:37 +0530

    converted to cra

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,11 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 function App() {
+	const [products, setProducts] = useState([]);
 	useEffect(() => {
 		const fetchData = async () => {
 			const response = await fetch('/products');
 			const json = await response.json();
+			setProducts(json.products);
 			console.log(json);
 		};
 		fetchData();
@@ -11,6 +13,7 @@ function App() {
 	return (
 		<section>
 			<h1>Hello</h1>
+			{products && products.map((product) => <p>{product.name}</p>)}
 		</section>
 	);
 }
