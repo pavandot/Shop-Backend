5762e0e1b096972e7dd8a8eb52a8bd9d4baa4806
pavan <pavandotnet007@outlook.com>
2022-06-08 19:47:50 +0530

    cros error fix

--- server/index.js
+++ server/index.js
@@ -20,6 +20,13 @@ const app = express();
 
 connectDB();
 
+// fix CORS error
+app.use((req, res, next) => {
+	res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+	res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+	next();
+});
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use('/uploads', express.static('uploads'));
