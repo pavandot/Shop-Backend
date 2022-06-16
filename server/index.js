046a29f04518caa4e0c1894c53ef3e151baa3b9b
pavan <pavandotnet007@outlook.com>
2022-06-16 22:36:09 +0530

    cors error

--- server/index.js
+++ server/index.js
@@ -24,6 +24,7 @@ connectDB();
 app.use((req, res, next) => {
 	res.header('Access-Control-Allow-Origin', '*');
 	res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+	res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH');
 	next();
 });
 
