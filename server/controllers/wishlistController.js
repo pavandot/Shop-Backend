6f9dacbf843d261f0aaf93741b2e47fbe8b69808
pavan <pavandotnet007@outlook.com>
2022-06-28 21:26:05 +0530

    added move to cart route

--- server/controllers/wishlistController.js
+++ server/controllers/wishlistController.js
@@ -1,6 +1,7 @@
 const asyncHandler = require('express-async-handler');
 
 const Wishlist = require('../models/wishlistModel.js');
+const Cart = require('../models/cartModel.js');
 
 const getWishlist = asyncHandler(async (req, res) => {
 	if (!req.user) {
@@ -86,8 +87,45 @@ const deleteWishlistItem = asyncHandler(async (req, res) => {
 	});
 });
 
+const moveToCart = asyncHandler(async (req, res) => {
+	if (!req.user) {
+		return res.status(401).json({
+			statusCode: 401,
+			message: 'Not authorized',
+		});
+	}
+
+	const wishlistItem = await Wishlist.findById(req.params.wishlistId);
+	if (!wishlistItem) {
+		return res.status(404).json({
+			statusCode: 404,
+			message: 'Wishlist item not found',
+		});
+	}
+
+	if (wishlistItem.user.toString() !== req.user.id) {
+		return res.status(401).json({
+			statusCode: 401,
+			message: 'Not authorized',
+		});
+	}
+
+	const cartBody = {
+		user: req.user.id,
+		product: req.body.product,
+		size: req.body.size,
+		quantity: req.body.quantity,
+	};
+	const cart = await Cart.create(cartBody);
+	await wishlistItem.remove();
+	res.status(200).json({
+		statusCode: 200,
+		cart,
+	});
+});
 module.exports = {
 	getWishlist,
 	addToWishlist,
 	deleteWishlistItem,
+	moveToCart,
 };
--- server/routes/wishlistRoutes.js
+++ server/routes/wishlistRoutes.js
@@ -1,10 +1,11 @@
 const { Router } = require('express');
-const { addToWishlist, getWishlist, deleteWishlistItem } = require('../controllers/wishlistController.js');
+const { addToWishlist, getWishlist, deleteWishlistItem, moveToCart } = require('../controllers/wishlistController.js');
 const protect = require('../middleware/authMiddleware.js');
 const router = Router();
 
 router.get('/', protect, getWishlist);
 router.post('/', protect, addToWishlist);
 router.delete('/:wishlistId', protect, deleteWishlistItem);
+router.post('/:wishlistId', protect, moveToCart);
 
 module.exports = router;
